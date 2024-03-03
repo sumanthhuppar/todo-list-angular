4b1cd0211beeb839f15b11be2ffafc87195888a9
Sumanth H <sumanthh30062002@gmail.com>
2024-03-03 20:49:37 +0530

    routes added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router'
 
 import { AppComponent } from './app.component';
 import { TodolistComponent } from './todolist/todolist.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
+
+
+const appRoutes: Routes = [
+{path:'', component:HomeComponent},
+{path:'about', component:AboutComponent},
+{path:'contact', component:ContactComponent},
+{path:'todolist',component:TodolistComponent}
+];
+
 
 @NgModule({
   declarations: [
@@ -18,7 +28,8 @@ import { AboutComponent } from './about/about.component';
   ],
   imports: [
     FormsModule,
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
